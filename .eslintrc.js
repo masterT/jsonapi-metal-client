b3031c90597731f44d18e4c66f653f1a24b86a88
Simon Thiboutôt <simonthiboutot@hotmail.com>
2022-02-07 07:53:27 -0500

    Setup Prettier.

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,14 +3,23 @@ module.exports = {
   parser: '@typescript-eslint/parser',
   plugins: [
     '@typescript-eslint',
+    'prettier'
   ],
   extends: [
     'eslint:recommended',
-    'plugin:@typescript-eslint/recommended'
+    'plugin:@typescript-eslint/recommended',
+    'prettier'
   ],
+  rules: {
+    "prettier/prettier": 2
+  },
   ignorePatterns: [
     ".eslintrc.js",
+    "jest.config.js",
     "node_modules/",
-    "dist/**/*"
+    "dist/**/*",
+    "docs/**/*",
+    "coverage/**/*",
+    "*.TypeGuards.ts"
   ]
 }
